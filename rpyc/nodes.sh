7d49dc10080abfb84126d69de236e81677045e84
Naoki Eto <neto@caltech.edu>
2017-11-14 11:58:15 -0800

    Added in a reset python script to reset the vertex files

--- rpyc/nodes.sh
+++ rpyc/nodes.sh
@@ -1,3 +1,5 @@
+python reset.py 10001 10002 10003
+
 python rpyc_server.py 10001 &
 FIRST=$!
 
@@ -8,7 +10,14 @@ SECOND=$!
 
 echo $SECOND
 
+python rpyc_server.py 10003 &
+THIRD=$!
+
+echo $THIRD
+
 python rpyc_client.py 10002 10001
+python rpyc_client.py 10001 10003
 
 kill $FIRST
 kill $SECOND
+kill $THIRD
