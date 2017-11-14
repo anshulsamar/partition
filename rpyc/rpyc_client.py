fd511d68658424f9fd1381fafa24d8d29638f5b2
Naoki Eto <neto@caltech.edu>
2017-11-14 09:21:07 -0800

    having trouble trying to get a machine to act as both a client and a server

--- rpyc/rpyc_client.py
+++ rpyc/rpyc_client.py
@@ -1,13 +1,33 @@
 import rpyc
 
+class ServerService(rpyc.Service):
+
+    def exposed_add_vertex(self, serverPort, vertexNum):
+      serverFile = open(str(serverPort) + "vertices.txt", "a")
+      serverFile.write(str(vertexNum) + "\n")
+      serverFile.close()
+
+
 class ClientService(rpyc.Service):
-    def exposed_hi(self):
-      print "hello!"
 
-def noisy(string):
-    print 'Noisy:', repr(string)
+    def exposed_remove_vertex(self, clientPort, vertexNum):
+      clientFileName = str(clientPort) + "vertices.txt"
+      clientFile = open(clientFileName, "r")
+      lines = clientFile.readlines()
+      lines.close()
+      
+      clientFile = open(clientFileName, "w")
+      for line in lines:
+        if int(line) != vertexNum:
+          clientFile.write(line)
+      clientFile.close()
 
-proxy = rpyc.connect('localhost', 18861, service=ClientService, config={'allow_public_attrs': True})
-fileobj = open('testfile.txt')
-linecount = proxy.root.line_counter(fileobj, noisy)
-print 'The number of lines in the file was', linecount
+from rpyc.utils.server import ThreadedServer
+t = ThreadedServer(ServerService, port = 20000)
+#t.start()
+#t.close()
+
+proxy = rpyc.connect('localhost', 10000, service=ClientService, config={'allow_public_attrs': True})
+
+proxy.root.add_vertex(10000, 2)
+
--- rpyc/rpyc_server.py
+++ rpyc/rpyc_server.py
@@ -1,12 +1,19 @@
 import rpyc
 
-class MyService(rpyc.Service):
+class ServerService(rpyc.Service):
+    '''
     def exposed_line_counter(self, fileobj, function):
         for linenum, line in enumerate(fileobj.readlines()):
             function(line)
         print self._conn.root.hi()
         return linenum + 1
+    '''
+
+    def exposed_add_vertex(self, serverPort, vertexNum):
+      serverFile = open(str(serverPort) + "vertices.txt", "a")
+      serverFile.write(str(vertexNum) + "\n")
+      serverFile.close()
 
 from rpyc.utils.server import ThreadedServer
-t = ThreadedServer(MyService, port = 18861)
+t = ThreadedServer(ServerService, port = 10000)
 t.start()
