e4f53b2f7f9aaf3f433f4429393af92594f76a5b
Naoki Eto <neto@caltech.edu>
2017-11-15 22:53:44 -0800

    gonna try to soldier on with rpyc, have an idea involving running client code then server code for each node that joins. More details are in the google doc

--- rpyc/rpyc_connection.py
+++ rpyc/rpyc_connection.py
@@ -15,15 +15,42 @@ class ClientService(rpyc.Service):
           clientFile.write(line)
       clientFile.close()
 
-if len(sys.argv) < 3:
-    print("Usage: python rpyc_client.py [client_port_num] [server_port_num] [vertex_to_move_from_client_to_server]")
-    exit()
+class OtherServerService(rpyc.Service):
 
-clientPort = int(sys.argv[1])
-serverPort = int(sys.argv[2])
-vertex = int(sys.argv[3])
+    # Add vertex to this server's file. Note that this also
+    # removes the vertex from the client's vertex file.
+    def exposed_add_vertex(self, serverPort, clientPort, vertexNum):
+        serverFile = open(str(serverPort) + "vertices.txt", "a")
+        serverFile.write(str(vertexNum) + "\n")
+        serverFile.close()
 
-proxy = rpyc.connect('localhost', serverPort, service=ClientService)#, config={'allow_public_attrs': True})
+        # Remove vertex from the client's vertex file
+        self._conn.root.remove_vertex(clientPort, vertexNum)
 
-proxy.root.add_vertex(serverPort, clientPort, vertex)
+def server_setup(portNum):
+    #print portNum
+    t = ThreadedServer(ServerService, port=portNum)
+    t.start()
+
+
+if __name__ == '__main__':
+
+    if len(sys.argv) < 3:
+        print("Usage: python rpyc_client.py [client_port_num] [server_port_num] [vertex_to_move_from_client_to_server]")
+        exit()
+
+    clientPort = int(sys.argv[1])
+    serverPortList = sys.argv[2:-1]#int(sys.argv[2])
+    # The vertex to move should be the last argument
+    vertex = int(sys.argv[-1])
+
+    # run client code (might need to do for loop so we can do multiple
+    # connections to multiple nodes) TODO
+    for serverPort in serverPortList:
+        proxy = rpyc.connect('localhost', int(serverPort), service=ClientService)#, config={'allow_public_attrs': True})
+
+        proxy.root.add_vertex(serverPort, clientPort, vertex)
+
+    # run server code
+    server_setup(clientPort)
 
